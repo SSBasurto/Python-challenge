051bad10c5e58abebc53359c96058bad0ec08643
Sergio Basurto <sergiosbasurto@gmail.com>
2022-04-23 23:31:32 -0400

    update

--- Py Poll/main.py
+++ Py Poll/main.py
@@ -1,1 +1,49 @@
+import csv
+import os
 
+#load election data and analysis output 
+file_to_load = os.path.join("Resources", "election_data.csv")
+file_to_output = os.path.join("analysis", "election_analysis.txt")
+
+#Total Vote Counter
+total_votes = 0
+
+#Candidate Options and Vote Counters
+candidate_options = []
+candidate_votes = {}
+
+#Winning Candidate and Winning Count 
+winning_candidate = ""
+winning_count = 0
+
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as election_data:
+    reader = csv.reader(election_data)
+
+    # Read the header
+    header = next(reader)
+
+    # For each row...
+    for row in reader:
+
+        # Run the loader animation
+       # print(". ", end=""),
+
+        # Add to the total vote count
+        total_votes = total_votes + 1
+
+        # Extract the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate doesn't match any existing candidate...
+        # (In a way, our loop is "discovering" candidates as it goes)
+        if candidate_name not in candidate_options:
+
+            # Add it to the list of candidates in the running
+            candidate_options.append(candidate_name)
+
+            # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
+
+        # Then add a vote to that candidate's count
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
